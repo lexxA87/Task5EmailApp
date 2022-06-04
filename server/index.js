a2b335776477fc375c22a9ab53595708391f9f7b
lexxA87 <alex.o.nikonov@gmail.com>
2022-06-04 21:11:30 +0400

    3. Added mongoDB and start server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
-console.log("YOOOOOO!!!!!");
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+app.listen(process.env.PORT, (error) => {
+  error
+    ? console.log(error)
+    : console.log("Server started on PORT ", process.env.PORT);
+});
+
+mongoose
+  .connect(process.env.MONGO_DB_URL)
+  .then((res) => console.log("Connected to DB"))
+  .catch((error) => console.log(error));
